184bf761dfbd844fca0577793535c3bec8c7f719
Bojan Antonovic <bojan@antonovic.ch>
2021-12-19 14:14:19 +0100

    cleanup

--- src/main/java/staticapplicationcontextdemo/Main.java
+++ src/main/java/staticapplicationcontextdemo/Main.java
@@ -6,23 +6,24 @@ import org.springframework.context.support.StaticApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
+        // see StaticApplicationContextDemoTest for a JUnit version
         try (StaticApplicationContext context = new StaticApplicationContext()) {
-            BeanFactory beanFactory = context;
+            var beanFactory = (BeanFactory) context;
 
-            final String default0 = "default0";
-            context.registerBean(default0, Foo.class);
-            System.out.println(beanFactory.getBean(default0));
-            System.out.println(context.isSingleton(default0));
+            final var defaultScopeId = "Foo in default scope";
+            context.registerBean(defaultScopeId, Foo.class);
+            System.out.println(beanFactory.getBean(defaultScopeId));
+            System.out.println(context.isSingleton(defaultScopeId));
 
-            final String singleton = "singleton";
-            context.registerSingleton(singleton, Foo.class);
-            System.out.println(beanFactory.getBean(singleton));
-            System.out.println(context.isSingleton(singleton));
+            final var singletonId = "Foo as singletonId";
+            context.registerSingleton(singletonId, Foo.class);
+            System.out.println(beanFactory.getBean(singletonId));
+            System.out.println(context.isSingleton(singletonId));
 
-            final String prototype = "prototype";
-            context.registerPrototype(prototype, Foo.class);
-            System.out.println(beanFactory.getBean(prototype));
-            System.out.println(context.isPrototype(prototype));
+            final var prototypeId = "Foo as prototypeId";
+            context.registerPrototype(prototypeId, Foo.class);
+            System.out.println(beanFactory.getBean(prototypeId));
+            System.out.println(context.isPrototype(prototypeId));
         }
     }
 
